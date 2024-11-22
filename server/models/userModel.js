f52293d79eb49c6d15c7c4af3f2a4512109ee3ed
venkay1227 <venkateshnaidu512@gmail.com>
2024-11-22 15:06:37 +0530

    commit

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -6,7 +6,7 @@ const userSchema = new mongoose.Schema({
     photo: { type: String, required: true },
     firstName: { type: String },
     lastName: { type: String },
-    creditBalance: { type: Number, default: 5 },
+    creditBalance: { type: Number, default: 5 }
 });
 
 const userModel = mongoose.models.user || mongoose.model("user", userSchema);
