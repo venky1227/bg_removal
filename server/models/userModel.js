5bc01ea663b43ceae8f2ff138e171117d10c47eb
venkay1227 <venkateshnaidu512@gmail.com>
2024-11-22 14:32:02 +0530

    Second Commit

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-const userSchmea = new mongoose.Schema({
+const userSchema = new mongoose.Schema({
     clerkId: {type:String, required: true, unique: true},
     email: {type: String, required: true, unique: true},
     photo: {type: String, required: true},
@@ -9,6 +9,6 @@ const userSchmea = new mongoose.Schema({
     creditBalance: {type: Number, default: 5},
 })
 
-const userModel = mongoose.models.user || mongoose.model("user", userSchmea)
+const userModel = mongoose.models.user || mongoose.model("user", userSchema)
 
 export default userModel;
