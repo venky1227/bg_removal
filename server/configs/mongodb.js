e1ce3369be3cd4777eb7bb76dd582748a274ae66
venkay1227 <venkateshnaidu512@gmail.com>
2024-11-23 13:01:32 +0530

    mongodb

--- server/configs/mongodb.js
+++ server/configs/mongodb.js
@@ -6,7 +6,7 @@ const connectDB = async () => {
     mongoose.connection.on('connected', () => {
         console.log('Database Connected');
     })
-    await mongoose.connect(`${process.env.MONGODB_URI}/bg-removal`);
+    await mongoose.connect(`${process.env.MONGODB_URI}/bg-removal`)
 }
 
 export default connectDB
