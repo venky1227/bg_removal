5d096ede8738eaebe13a02d7fa31c74098cf4612
venkay1227 <venkateshnaidu512@gmail.com>
2024-11-25 10:00:19 +0530

    env updated

--- server/configs/mongodb.js
+++ server/configs/mongodb.js
@@ -1,26 +1,26 @@
-// import mongoose from "mongoose";
+import mongoose from "mongoose";
 
-
-// const connectDB = async () => {
-
-//     mongoose.connection.on('connected', () => {
-//         console.log('Database Connected');
-//     })
-//     await mongoose.connect(`${process.env.MONGODB_URI}/bg-removal`)
-// }
-
-// export default connectDB
-
-import mongoose from 'mongoose';
 
 const connectDB = async () => {
-  try {
-    await mongoose.connect(`${process.env.MONGODB_URI}/bg-removal`);
-    console.log('MongoDB connected successfully');
-  } catch (error) {
-    console.error('MongoDB connection error:', error);
-    process.exit(1);
-  }
-};
 
-export default connectDB;
+    mongoose.connection.on('connected', () => {
+        console.log('Database Connected');
+    })
+    await mongoose.connect(`${process.env.MONGODB_URI}/bg-removal`)
+}
+
+export default connectDB
+
+// import mongoose from 'mongoose';
+
+// const connectDB = async () => {
+//   try {
+//     await mongoose.connect(`${process.env.MONGODB_URI}/bg-removal`);
+//     console.log('MongoDB connected successfully');
+//   } catch (error) {
+//     console.error('MongoDB connection error:', error);
+//     process.exit(1);
+//   }
+// };
+
+// export default connectDB;
