323be88b5097eaf0396a0334f4ee347a1f0155be
venkay1227 <venkateshnaidu512@gmail.com>
2024-11-26 10:22:07 +0530

    userRouter

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 import 'dotenv/config';
 import express from 'express'
 import cors from 'cors'
-// import userRouter from './routes/userRoutes.js';
+import userRouter from './routes/userRoutes.js';
 import connectDB from './configs/mongodb.js';
 // import imageRouter from './routes/imageRoutes.js';
 
@@ -15,7 +15,7 @@ app.use(express.json())
 app.use(cors())
 
 // API routes
-// app.use('/api/user',userRouter)
+app.use('/api/user',userRouter)
 // app.use('/api/image',imageRouter)
 
 app.get('/', (req,res) => res.send("API Working"))
